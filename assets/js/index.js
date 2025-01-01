68eb9d8bc137afe31abac2feeabb5bc048459b2b
Elayabarathi <elayabarathiedison@gmail.com>
2025-01-01 20:10:01 +0530

    link updating 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,7 @@ $.map(unindexed_array, function(n, i){
     data[n['name']]=n['value']
 })
     var request = {
-        "url": `https://crud-app-gamma-pied.vercel.app/api/users/${data.id}`,
+        "url": `http://localhost:3000/api/users/${data.id}`,
         "method": "PUT",
         "data": data
     }
@@ -29,7 +29,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url": `https://crud-app-gamma-pied.vercel.app/api/users/${id}`,
+            "url": `http://localhost:3000/api/users/${id}`,
             "method": "DELETE"
         }
 
